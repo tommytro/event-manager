def52a2aad262db66410ba631672eefe9b071dc6
tommytro <85188391+tommytro@users.noreply.github.com>
2023-02-22 23:43:37 -0700

    changed some things

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<div>
+    <ul>
+        <li><a href="create_event.html" class="navbar">Create Event</a></li>
+        <li><a href="friends.html" class="navbar">Friends</a></li>
+        <li><a href="events.html" class="navbar">Events</a></li>
+        <li><a href="statistics.html" class="navbar">Co-Planners</a></li>
+    </ul>
+</div>
