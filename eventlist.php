5d52c02027e020f3d0b5845e6e34ada795b81de6
tommytro <85188391+tommytro@users.noreply.github.com>
2023-02-25 13:21:20 -0700

    update

---
+++ eventlist.php
@@ -0,0 +1,6 @@
+<div>
+    <ul class"friendslistUL">
+        <li class="friendslistLI">Jimmy's Birthday 1/13/2024</li>
+        <li class="friendslistLI">Agnes Graduation 5/17/2023</li>
+    </ul>
+</div>
---
+++ events.php
@@ -0,0 +1,19 @@
+<html>
+    <link rel="stylesheet" href="homepage.css">
+    <title>Event Manager Project</title>
+    <link rel="icon" type"image/x-icon" href="favicon.ico"/> 
+    <head>
+        <form style="float: right" > 
+            <button  class="button">Logged In</button>
+        </form>   
+    <?php include("logo.php"); ?>
+    
+    </head>
+    <body>
+        <?php include("navbar.php"); ?>
+
+        <h3 class="friendsContainer">Friends List: <?php include("eventlist.php"); ?></h3>
+        
+
+    </body>
+</html>
