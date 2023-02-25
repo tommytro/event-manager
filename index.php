4c5178538222500a49819bb77e5aa5ff22dbd311
tommytro <85188391+tommytro@users.noreply.github.com>
2023-02-24 22:20:46 -0700

    added favicon

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <html>
     <link rel="stylesheet" href="homepage.css">
+    
     <head>
-    <form style="float: right" action="logged_in_homepage.php"> 
-        <button  class="button" onclick="logged_in_homepage.php">Login</button>
-    </form>   
-    <?php include("logo.php"); ?>
+        <title>Event Manager Project</title>
+        <link rel="icon" type"image/x-icon" href="favicon.ico"/> 
+        <form style="float: right" action="logged_in_homepage.php"> 
+            <button  class="button" onclick="logged_in_homepage.php">Login</button>
+        </form>   
+        <?php include("logo.php"); ?>
    
         
     </head>
