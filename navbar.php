4f73013b660052598b8fb68dd048be78f7a78158
tommytro <85188391+tommytro@users.noreply.github.com>
2023-02-25 13:24:47 -0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -3,6 +3,6 @@
         <li><a href="create_event.php" class="navbar">Create Event</a></li>
         <li><a href="friends.php" class="navbar">Friends</a></li>
         <li><a href="events.php" class="navbar">Events</a></li>
-        <li><a href="statistics.php" class="navbar">Co-Planners</a></li>
+       
     </ul>
 </div>
