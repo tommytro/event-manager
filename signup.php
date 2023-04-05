a32be5bb134b6c832ad699ac71624c0156efd77d
tommytro <85188391+tommytro@users.noreply.github.com>
2023-04-05 07:20:48 -0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -126,7 +126,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
             </div>
             <p>Already have an account? <a href="login.php">Login here</a>.</p>
         </form>
